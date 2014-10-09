40cf62fdb6a07f0cba17d6cf460b788814c6c85f
Anna Michelle <anna_michelle@msn.com>
2014-10-09 12:47:22 -0500

    deleted commented section

--- themes/emiglio_remix/custom.php
+++ themes/emiglio_remix/custom.php
@@ -14,15 +14,7 @@ function emiglio_exhibit_builder_page_nav($exhibitPage = null)
     foreach ($pages as $page) {
         $current = (exhibit_builder_is_current_page($page)) ? 'class="current"' : '';
         $html .= "<li $current>" . exhibit_builder_link_to_exhibit($exhibit, $page->title, array(), $page);
-        /* if ($page->countChildPages() > 0) {
-            $childPages = $page->getChildPages();
-            $html .= '<ul class="child-pages">';
-                foreach ($childPages as $childPage) {
-                    $current = (exhibit_builder_is_current_page($childPage)) ? 'class="current"' : '';
-                    $html .= "<li $current>" . exhibit_builder_link_to_exhibit($exhibit, $childPage->title, array(), $childPage) . '</li>';
-                }
-            $html .= '</ul>';
-        } */
+        
         $html .='</li>';
     }
     $html .= '</ul>' . "\n";
